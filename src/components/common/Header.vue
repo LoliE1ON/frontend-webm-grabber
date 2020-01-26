0d7423c37bab3ebdf4981031105f95c8078b4bef
sdf <trohimovkavad@gmail.com>
2020-01-26 15:52:00 +0200

    add html skeleton

---
+++ src/components/common/Header.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <Bar v-bind:draw="drawBar"/>
+
+        <v-app-bar app color="indigo" dark>
+            <v-app-bar-nav-icon @click="drawBar = !drawBar" />
+            <v-toolbar-title>Application</v-toolbar-title>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+    import Bar from "./Bar"
+    export default {
+        data: function () {
+            return {
+                drawBar: true,
+            }
+        },
+        components: {
+            Bar,
+        },
+    }
+</script>
